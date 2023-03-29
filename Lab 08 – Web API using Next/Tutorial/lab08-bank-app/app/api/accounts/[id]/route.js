2a11412b3ea1cd3ceddddd2e331fbea0d129c074
Ziad-za1905466 <za1905466@qu.edu.qa>
2023-03-29 16:05:48 +0300

    Update route.js

--- Lab 08 – Web API using Next/Tutorial/lab08-bank-app/app/api/accounts/[id]/route.js
+++ Lab 08 – Web API using Next/Tutorial/lab08-bank-app/app/api/accounts/[id]/route.js
@@ -7,3 +7,16 @@ export async function GET(request, { params }) {
     const account = await repo.getAccount(id)
     return Response.json(account, { status: 200 });
 }
+
+export async function POST(request) {
+    const account = await request.json();
+    const newAccount = await repo.addAccount(account)
+    return Response.json(newAccount)
+
+}
+export async function PUT(request) {
+    const account = await request.json();
+    const newAccount = await repo.updateAccount(account)
+    return Response.json(newAccount)
+    
+}
